6b576be9c891cc384c60e4d2e0376e60d45119af
nullrequest <30698906+advaithm@users.noreply.github.com>
2021-07-19 17:49:26 +0530

    escape paths with regex over quotes in flatpaks (#558)

--- app/src/lib/helpers/linux.ts
+++ app/src/lib/helpers/linux.ts
@@ -28,7 +28,9 @@ export function convertToFlatpakPath(path: string) {
 
   return join('/var/run/host', path)
 }
-
+export function formatWorkingDirectoryForFlatpak(path: string): string {
+  return path.replace(/(\s)/, "\ ")
+}
 /**
  * Checks the file path on disk exists before attempting to launch a specific shell
  *
@@ -82,9 +84,10 @@ export function spawnEditor(
   options: SpawnOptions
 ): ChildProcess {
   if (isFlatpakBuild()) {
+    let EscapedworkingDirectory = formatWorkingDirectoryForFlatpak(workingDirectory)
     return spawn(
       'flatpak-spawn',
-      ['--host', path, `"${workingDirectory}"`],
+      ['--host', path, EscapedworkingDirectory],
       options
     )
   } else {
